6361b15337b0befe3c167ce549bc119107c4c844
skprasad117 <skprasad117@gmail.com>
2023-07-20 19:35:55 +0530

    removed unnessary lines

--- multipleimagesapp/views.py
+++ multipleimagesapp/views.py
@@ -12,25 +12,20 @@ def home(request):
 def uploadhandler(request):
     if request.method == "POST":
         images_id = request.POST.get("image_id")
-       
-        print("---------------->", images_id)
+    
         if images_id:
-            print("------------")
             gallery_instance = Gallery.objects.get(pk=images_id)
         else:
-            print("i am running")
             title = request.POST.get('title')
             gallery_instance = Gallery.objects.create(title=title)
                 
         
         files = request.FILES.getlist('images')
         if files:
-            # gallery_instance = Gallery.objects.create(title=title)
+            
 
             for count,file in enumerate(files):
                 try:
-                    # new_name = str(gallery_instance.pk) +"-"+ file.name
-                    # print(file.name, new_name)
                     saved_file_instance = fs.save(file.name,file)
                 except Exception as e:
                     print(f"Exception : {e}")
@@ -54,11 +49,9 @@ def gallery(request,pk):
     data = dict()
     
     if gallery_instance:
-        # print("running", gallery_instance)
         data["title"] = gallery_instance[0].title
         if gallery_instance[0].images:
             data["images"] = gallery_instance[0].images.split(",")
-        # print(data)
         return render(request, "multipleimagesapp/gallery.html",{'data_uid':pk, "data":data})
     message = "Either instance not exist or wrong request made"
     return redirect(reverse("multipleimagesapp:home"))
@@ -68,9 +61,7 @@ def delete(request):
         uid = request.POST["uid"]
         file_name = request.POST["image"]
         gallery_instance = Gallery.objects.filter(pk=uid)
-        print("running delete",gallery_instance)
         if gallery_instance:
-            print("running if")
             try:
                 file_name_delete =file_name
 
@@ -78,28 +69,19 @@ def delete(request):
                     if len(gallery_instance[0].images.split(","))!=1:
                         file_name_delete = ","+file_name_delete 
                 else:
-                    print("not at end")
                     file_name_delete = file_name_delete + ","
-                    print("file name to del", file_name_delete)
                 gallery_instance[0].images = gallery_instance[0].images.replace(file_name_delete,"")
                 gallery_instance[0].save()
-                print(file_name)
-                print("images",gallery_instance[0].images)
             except Exception as e:
                 print(f"Exception : {e}")
             else:
                 file_name = file_name.split("/")[-1]
                 file_name = file_name.replace("%20"," ")
-                print(file_name,type(file_name))
-                print("------------------>",file_name)
                 try:
                     pass
                     fs.delete(file_name)
                 except Exception as e:
-                    print("------------------------")
-                    print(e)
-                # print("sadas0",file_name.split("/")[-1])
-                print("---------------")
+                    print(f"Exception : {e}")
         return redirect(reverse('multipleimagesapp:gallery',args=(uid,)))
     return redirect(reverse("multipleimagesapp:home"))
 
@@ -113,7 +95,7 @@ def update(request):
             saved_file_instance = fs.save(file.name,file)
             gallery_instance.images = gallery_instance.images.replace(image,fs.url(saved_file_instance))
         except Exception as e:
-            print(e)
+            print(f"Exception : {e}")
         else:
             gallery_instance.save()
             file_name = image.split("/")[-1]
@@ -122,5 +104,3 @@ def update(request):
         return redirect(reverse('multipleimagesapp:gallery',args=(uid,)))
     return redirect(reverse("multipleimagesapp:home"))
 
-def extend(request):
-    pass
